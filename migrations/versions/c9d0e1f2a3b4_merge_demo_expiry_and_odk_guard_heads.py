ae3020b431993606be5b0ca2fdb5c3352f613b68
Vivek Gupta <drguptavivek@yahoo.com>
2026-03-16 00:08:01 +1100

    Merge Alembic heads after demo expiry

---
+++ migrations/versions/c9d0e1f2a3b4_merge_demo_expiry_and_odk_guard_heads.py
@@ -0,0 +1,21 @@
+"""merge demo expiry and odk guard heads
+
+Revision ID: c9d0e1f2a3b4
+Revises: b4c5d6e7f8a9, b7c8d9e0f1a2
+Create Date: 2026-03-16 09:15:00.000000
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "c9d0e1f2a3b4"
+down_revision = ("b4c5d6e7f8a9", "b7c8d9e0f1a2")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
